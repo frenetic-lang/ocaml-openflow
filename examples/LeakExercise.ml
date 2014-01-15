92fb6b08ac9213e6e8fa96244ff4730281a1612f
Arjun Guha <arjun@cs.umass.edu>
2014-01-14 23:07:16 -0500

    This version seems to leak.

--- examples/LeakExercise.ml
+++ examples/LeakExercise.ml
@@ -8,8 +8,11 @@ open OpenFlow0x01.Message
 module OF0x01Controller = Async_OpenFlow0x01.Controller
 module Log = Async_OpenFlow.Log
 
-let f = function
-  | `Connect client_id -> Log.info "connect"; return ()
+let f t = function
+  | `Connect client_id ->
+      OF0x01Controller.send t client_id (0l, Hello (Cstruct.of_string ""))
+        >>= fun _ ->
+        return ()
   | `Message _ -> return ()
   | `Disconnect (client_id, _) -> Log.info "disconnect"; return ()
 
@@ -17,7 +20,7 @@ let main () =
   let open OF0x01Controller in
   create 6633
   >>= fun t ->
-  Pipe.iter (listen t) ~f:f
+  Pipe.iter (listen t) ~f:(f t)
 
 let _ = main ()
 let _ = never_returns (Scheduler.go ())
