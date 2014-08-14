8eb5f3fa5e7676d1607d9e89d7596384302f3be4
Spiros Eliopoulos <seliopou@gmail.com>
2014-08-14 16:58:02 -0400

    txn: Add support for RPC-like messages to OF0x04 controller

--- async/Async_OpenFlow0x04.ml
+++ async/Async_OpenFlow0x04.ml
@@ -110,6 +110,10 @@ module Controller = struct
     let c_id = client_id_of_switch_exn t sw_id in
     ChunkController.send t.sub c_id (Message.marshal' msg)
 
+  let send_txn t sw_id msg =
+    let c_id = client_id_of_switch_exn t sw_id in
+    ChunkController.send_txn t.sub c_id (Message.marshal' msg)
+
   let send_ignore_errors t sw_id msg =
     let c_id = client_id_of_switch_exn t sw_id in
     ChunkController.send_ignore_errors t.sub c_id (Message.marshal' msg)
@@ -197,7 +201,7 @@ module Controller = struct
     let open ChunkController in
     let stages =
       (local (fun t -> t.sub)
-        (handshake 0x04 >=> echo))
+        (echo >=> txn >=> handshake 0x04))
       >=> openflow0x04
       >=> features in
     run stages t (listen t.sub)
